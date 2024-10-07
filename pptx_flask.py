3dcb7d36794e472bf57a8e81eddbf4cd1a558dea
xhsyni <xuanhanchin@gmail.com>
2024-10-07 11:37:04 +0800

    Update pptx_flask.py

--- pptx_flask.py
+++ pptx_flask.py
@@ -17,7 +17,7 @@ def convert_to_pdf(input_file, output_file):
     try:
         output_dir = os.path.dirname(output_file)
         subprocess.run([
-            'libreoffice',
+            'soffice.exe',
             '--headless',
             '--convert-to', 'pdf',
             '--outdir', output_dir,  # Output directory for the PDF
